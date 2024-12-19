71b91de2c1ac467cca88c31e9bb90a97e25883b8
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-12-19 22:52:02 +0100

    -Added tree view functionality -Enhanced tooltip functionality with additional parameter options -Improved efficiency and reliability of basic components -Improved user experience

--- src/Components/Base/RgfHtml.cs
+++ src/Components/Base/RgfHtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Recrovit.RecroGridFramework.Client.Blazor.UI.Components.Base;
 
@@ -10,6 +11,15 @@ public class RgfHtml : RgfBaseComponent
 
     [Parameter]
     public RenderFragment ChildContent { get; set; } = null!;
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnDblClick { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnContextMenu { get; set; }
 
     protected RenderFragment? Content { get; set; }
 
@@ -29,6 +39,22 @@ public class RgfHtml : RgfBaseComponent
                 builder.AddAttribute(sequence++, attribute.Key, attribute.Value);
             }
         }
+
+        if (OnClick.HasDelegate)
+        {
+            builder.AddAttribute(sequence++, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, OnClick));
+        }
+
+        if (OnDblClick.HasDelegate)
+        {
+            builder.AddAttribute(sequence++, "ondblclick", EventCallback.Factory.Create<MouseEventArgs>(this, OnDblClick));
+        }
+
+        if (OnContextMenu.HasDelegate)
+        {
+            builder.AddAttribute(sequence++, "oncontextmenu", EventCallback.Factory.Create<MouseEventArgs>(this, OnContextMenu));
+        }
+
         builder.AddElementReferenceCapture(sequence++, capturedRef => _elementReference = capturedRef);
         if (ChildContent != null)
         {
